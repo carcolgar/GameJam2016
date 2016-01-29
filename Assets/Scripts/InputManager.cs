eb4e9675e240493105d7398d35122b0278cbbf08
Carlos Colomé García <carcolgar@gmail.com>
2016-01-29 23:08:40 +0100

    - Creada carpeta para escenas de maquetación - Subido plugin para VisualStudio Code - Manager del input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour, IPointerClickHandler
+{
+       
+    /// <summary>
+    /// Handler para clicks del usuario
+    /// </summary>
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        /*
+        print("I was clicked");
+        target = Color.blue;
+        */
+    }
+	
+}
