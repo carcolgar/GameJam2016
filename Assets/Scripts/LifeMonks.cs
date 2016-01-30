5b6474f477b4d6119a9cb32287c4b1330c04c1f8
Fran <fran.kun@gmail.com>
2016-01-30 09:12:40 +0100

    Escena main con monjes

--- Assets/Scripts/LifeMonks.cs
+++ Assets/Scripts/LifeMonks.cs
@@ -10,25 +10,32 @@ public class LifeMonks : MonoBehaviour {
 	/// True -> No mira a cámara (feliz)
 	/// False -> Mira a cámara (enfadado)
 	/// </summary>
-	private List<bool> _lifeMonks;
+	private GameObject[] _lifeMonks;
+	private bool[] _happyMonks;
 
 	/// <summary>
 	/// Número de monjes que no te miran
 	/// </summary>
-	private int _happyMonks;
+	private int _numHappyMonks;
 
 	/// <summary>
 	/// Número de monjes que sí te miran
 	/// </summary>
-	private int _angryMonks;
+	private int _numAngryMonks;
 	#endregion
 
 	#region UNITY
 	// Use this for initialization
 	void Start () {
-		_lifeMonks = new List<bool>(GameManager.SINGLETON.life);
-		_happyMonks = GameManager.SINGLETON.life;
-		_angryMonks = 0;
+		_lifeMonks = GameObject.FindGameObjectsWithTag("LifeMonk");
+		//_happyMonks = new bool[GameManager.SINGLETON.life];
+		_numHappyMonks = GameManager.SINGLETON.life;
+		_numAngryMonks = 0;
+
+		//for (int i = 0; i < GameManager.SINGLETON.life; i++) {
+		//	Debug.Log (":)");
+		//	_happyMonks [i] = true;
+		//}
 	}
 	
 	// Update is called once per frame
@@ -43,14 +50,15 @@ public class LifeMonks : MonoBehaviour {
 	/// </summary>
 	public void LifeLost() {
 		//Elegimos un monje feliz random para ponerlo enfadado
-		int index = Random.Range (0, _happyMonks);
+		int index = Random.Range (0, _numHappyMonks);
 		int count = 0;
 
 		//Buscamos al monje que hemos elegido y hacemos que mire a cámara
-		for (int i=0; i<_lifeMonks.Count; i++) {
-			if (_lifeMonks[i] == true) {
+		for (int i=0; i<_lifeMonks.Length; i++) {
+			if (_happyMonks[i] == true) {
 				if (count == index) {
-					//TODO Turn Around
+					_happyMonks[i] = false;
+					Debug.Log("El monje " + i + " se ha enfadado");
 					break;
 				}
 				count++;
@@ -58,8 +66,8 @@ public class LifeMonks : MonoBehaviour {
 		}
 
 		//Ahora hay un monje enfadado más y un monje feliz menos
-		_angryMonks++;
-		_happyMonks--;
+		_numAngryMonks++;
+		_numHappyMonks--;
 	}
 
 
@@ -68,13 +76,14 @@ public class LifeMonks : MonoBehaviour {
 	/// </summary>
 	public void LifeGained() {
 		//Elegimos un monje enfadado random para ponerlo feliz
-		int index = Random.Range (0, _angryMonks);
+		int index = Random.Range (0, _numAngryMonks);
 		int count = 0;
 
 		//Buscamos al monje que hemos elegido y hacemos que mire palante
-		for (int i=0; i<_lifeMonks.Count; i++) {
-			if (_lifeMonks[i] == false) {
+		for (int i=0; i<_lifeMonks.Length; i++) {
+			if (_happyMonks[i] == false) {
 				if (count == index) {
+					_happyMonks[i] = true;
 					//TODO Turn Around
 					break;
 				}
@@ -83,24 +92,8 @@ public class LifeMonks : MonoBehaviour {
 		}
 
 		//Ahora hay un monje feliz más y un monje enfadado menos
-		_happyMonks++;
-		_angryMonks--;
-	}
-	#endregion
-
-	#region SINGLETON
-	// Unica instancia de la clase
-	private static GameManager _instance = null;
-
-	/// <summary>
-	/// Propiedad para acceder al singleton
-	/// </summary>
-	public static GameManager SINGLETON {
-		get {
-			if (_instance == null)
-				Debug.LogError("ERROR OMG!!!");
-			return _instance;
-		}
+		_numHappyMonks++;
+		_numAngryMonks--;
 	}
 	#endregion
 }
